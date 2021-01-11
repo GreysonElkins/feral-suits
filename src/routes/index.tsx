c10f7a344d2dd31f03db82516dad965c64b0e56d
Greyson Elkins <greysonelkins@gmail.com>
2021-01-11 13:17:41 -0700

    Combine routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,15 @@
+import aboutRoute from './about'
+import adminRoute from './admin'
+import eventsRoute from './events'
+import homeRoute from './home'
+import musicRoute from './music'
+
 export type route = {
   path: string
   public: boolean
   component: React.FC
 }
+
+const allRoutes = Object.assign(aboutRoute, adminRoute, eventsRoute, homeRoute, musicRoute)
+
+export default allRoutes
