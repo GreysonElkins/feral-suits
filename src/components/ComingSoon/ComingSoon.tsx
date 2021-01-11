62cb0d3ad2877fe2eb058b7247ae000718de4c51
Greyson Elkins <greysonelkins@gmail.com>
2021-01-11 14:35:05 -0700

    Create coming soon component

---
+++ src/components/ComingSoon/ComingSoon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ComingSoon:React.FC = () => {
+  return (
+    <h1>Coming Soon</h1>
+  )
+}
+
+export default ComingSoon
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,10 +1,11 @@
 import Home from '../components/Home/Home'
+import ComingSoon from '../components/ComingSoon/ComingSoon'
 import { route } from './index'
 
 const homeRoute:route = {
   path: '/',
   public: true,
-  component: Home
+  component: ComingSoon
 }
 
 export default homeRoute
