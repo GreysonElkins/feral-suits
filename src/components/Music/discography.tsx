a616b230d229782c829e6631be4940257b7dcb49
Greyson Elkins <greysonelkins@gmail.com>
2021-01-13 13:51:54 -0700

    Add singles data

--- src/components/Music/discography.tsx
+++ src/components/Music/discography.tsx
@@ -1,4 +1,7 @@
-import { ReactNode } from "react"
+import traipseArt from '../../assets/images/traipse.jpg'
+import spokaneArt from '../../assets/images/spokane.jpg'
+
+import { traipseLyrics, spokaneLyrics } from './lyrics'
 
 class Track { 
   name: string
@@ -18,11 +21,13 @@ class Track {
   }
 
   addLyrics = (lyrics:string[]) => {
+    this.lyrics = lyrics
+  }
 
+  printDuration = () => {
+    return `${Math.floor(this.duration / 60)}:${this.duration % 60}`
   }
 }
-
-// const traipseOfYouth = new Track("Traipse of Youth", "") 
 
 class Album {
   name: string
@@ -47,19 +52,80 @@ class Album {
       this.type = type
     }
 
-    addTrack = (track:Track) => {
-      this.tracks.push(track)
-    }
+  addTrack = (track:Track) => {
+    this.tracks.push(track)
+  }
 
-    addTracks = (tracks:Track[]) => {
-      this.tracks = this.tracks.concat(tracks)
-    }
+  addTracks = (tracks:Track[]) => {
+    this.tracks = this.tracks.concat(tracks)
+  }
+
+  printDuration = ():string => {
+    const inSeconds = this.tracks.reduce((totalTime, track):number => {
+      totalTime += track.duration
+      return totalTime
+    }, 0) 
+
+    return `${Math.floor(inSeconds / 60)}:${inSeconds % 60}`
+  }
 }
 
+class Discography {
+  albums: Album[]
+  constructor() {
+    this.albums = []
+  }
 
+  addAlbum = (album:Album) => {
+    this.albums.push(album)
+  }
+}
 
-const discography:Album[] = [
+const feralAlbums = new Discography() 
 
-]
+const traipseOfYouth = new Track("Traipse of Youth", 0, 248) 
+traipseOfYouth.addLyrics(traipseLyrics)
+
+const traipseSingle = new Album(
+  "Traipse of Youth", 
+  "July 20 2018", 
+  <iframe 
+    style={{border: "0", width: "350px", height: "442px"}} 
+    src="https://bandcamp.com/EmbeddedPlayer/track=1173907805/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" 
+    seamless
+  >
+    <a href="https://feralsuits.bandcamp.com/track/traipse-of-youth">
+      Traipse of Youth by Feral Suits
+    </a>
+  </iframe>,
+  traipseArt,
+  "single"
+   )
+
+  traipseSingle.addTrack(traipseOfYouth)
+
+  feralAlbums.addAlbum(traipseSingle)
+
+  const spokane = new Track("Spokane & Poughkeepsie", 0, 168)
+  spokane.addLyrics(spokaneLyrics)
+  const spokaneSingle = new Album(
+    "Spokane & Poughkeepsie", 
+    "January 20, 2020", 
+    <iframe 
+      style={{border: 0, width: "350px", height: "442px"}} 
+      src="https://bandcamp.com/EmbeddedPlayer/track=806203346/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" 
+      seamless
+    >
+      <a href="https://feralsuits.bandcamp.com/track/spokane-poughkeepsie">
+        Spokane &amp; Poughkeepsie by Feral Suits
+      </a>
+    </iframe>,
+    spokaneArt,
+    "single"
+    ) 
+    
+  spokaneSingle.addTrack(spokane)
+
+  feralAlbums.addAlbum(spokaneSingle)
 
 export {}
